973a730a0b6ebc74041f9f7e0a0a36a9f9de595c
bryan mcwatters <bmcwatters@comcast.net>
2021-12-13 11:35:20 -0500

    initial commit

---
+++ ListDataAccess.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryPatternExercise2
+{
+    public class ListDataAccess : IDataAccess
+    {
+        public static List<Product> Products = new List<Product>()
+        {
+            new Product() { Name = "xbox", Price = 350},
+            new Product() { Name = "laptop", Price = 900},
+            new Product() { Name = "guitar", Price = 450},
+            new Product(){ Name = "Blender", Price = 125},
+        };
+
+
+        public List<Product> LoadData()
+        {
+            Console.WriteLine("I am reading data from List Data Acess.");
+            return Products;
+
+
+        }
+
+        public void SavaData()
+        {
+            Console.WriteLine("I am saving data from a List Data Access");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Threading;
+
+namespace FactoryPatternExercise2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string userInput;
+            bool correctInput;
+            do {
+                Console.Clear();
+                correctInput = true;
+
+                Console.WriteLine("What database would you like to use?");
+                Console.WriteLine("Type: sql, mongo or list");
+                userInput = Console.ReadLine();
+
+                if(userInput != "sql" && userInput != "mongo" && userInput != "list")
+                {
+                    correctInput = false;
+                    Console.WriteLine("Bad Input!");
+                    Thread.Sleep(1000);
+                }
+
+            } while (!correctInput); ; 
+               Console.Clear ();
+
+
+            IDataAccess db = DataAccessFactory.GetDataAccessType(userInput);
+            var products = db.LoadData();
+            db.SavaData();
+
+            foreach(var product in products)
+            {
+                Console.WriteLine($"Name: {product.Name} {product.Price}");
+
+            }
+
+
+
+        }
+    }
+}
